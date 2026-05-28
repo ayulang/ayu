519b2a359f2f4205b19f49ca960bb60991b034af
BaxoPlenty <60827021+BaxoPlenty@users.noreply.github.com>
2026-05-28 11:28:24 +0200

    feat: basic lexer struct

--- compiler/ayuc_lexer/src/lib.rs
+++ compiler/ayuc_lexer/src/lib.rs
@@ -1,1 +1,17 @@
+use ayuc_scanner::Scanner;
 
+pub struct Lexer<'a> {
+    /// The source string. Reserved for error diagnostics.
+    source: &'a str,
+    /// The token scanner.
+    scanner: Scanner<'a>,
+}
+
+impl<'a> Lexer<'a> {
+    pub fn new(source: &'a str) -> Self {
+        Self {
+            source,
+            scanner: Scanner::new(source),
+        }
+    }
+}
