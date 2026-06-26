4240fbb5609dbfe1b8fe3b10b0559b7938fd5db2
BaxoPlenty <60827021+BaxoPlenty@users.noreply.github.com>
2026-06-26 20:04:02 +0200

    feat: add `PrimTy`

--- compiler/ayuc_hir/src/ty.rs
+++ compiler/ayuc_hir/src/ty.rs
@@ -1,4 +1,25 @@
+use ayuc_span::symbol::Symbol;
+
 #[derive(Debug)]
 pub enum Ty {
     Unit,
+    Primitive(PrimTy),
+}
+
+#[derive(Debug)]
+pub enum PrimTy {
+    Integer,
+    Str,
+}
+
+impl PrimTy {
+    pub fn from_name(sym: Symbol) -> Option<Self> {
+        let prim = match sym.as_str() {
+            "int" => Self::Integer,
+            "str" => Self::Str,
+            _ => return None,
+        };
+
+        Some(prim)
+    }
 }
