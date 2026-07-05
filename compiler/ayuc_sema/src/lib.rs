e473ff6daf5c1255a336375ac5f1c8e4d3b7ab87
BaxoPlenty <60827021+BaxoPlenty@users.noreply.github.com>
2026-07-05 11:03:22 +0200

    feat: basic `SemanticAnalyzer` foundation

--- compiler/ayuc_sema/src/lib.rs
+++ compiler/ayuc_sema/src/lib.rs
@@ -1,1 +1,10 @@
+use ayuc_ast::Ast;
+use ayuc_resolve::Resolver;
 
+pub struct SemanticAnalyzer;
+
+impl SemanticAnalyzer {
+    pub fn typecheck(_ast: &Ast, _resolver: &Resolver) {
+        todo!()
+    }
+}
