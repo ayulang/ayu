f7aa855d15b20d0c48147e7f50fd47418021b877
BaxoPlenty <60827021+BaxoPlenty@users.noreply.github.com>
2026-07-20 18:28:28 +0200

    feat(session): add visibility for items

--- compiler/ayuc_session/src/item.rs
+++ compiler/ayuc_session/src/item.rs
@@ -3,10 +3,28 @@ use std::collections::HashMap;
 use ayuc_id::{ast::NodeId, hir::DefId};
 use ayuc_span::{Span, symbol::Symbol};
 
+#[derive(PartialEq, Eq)]
+pub enum Visibility {
+    Public,
+    Private,
+}
+
 pub struct ItemInfo {
     pub name: Symbol,
     pub kind: ItemKind,
     pub id: NodeId,
+    pub vis: Visibility,
+}
+
+impl ItemInfo {
+    pub fn signature_span(&self) -> Span {
+        match &self.kind {
+            ItemKind::ExternFn { signature_span, .. }
+            | ItemKind::Fn { signature_span, .. }
+            | ItemKind::InlineMod { signature_span, .. }
+            | ItemKind::ExternMod { signature_span, .. } => *signature_span,
+        }
+    }
 }
 
 pub enum ItemKind {
