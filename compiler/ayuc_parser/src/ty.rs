2d5484d291fe3540e94c488ae0c71db91c90f16d
BaxoPlenty <60827021+BaxoPlenty@users.noreply.github.com>
2026-07-21 12:14:15 +0200

    feat(parser): parsing for tuple types

--- compiler/ayuc_parser/src/ty.rs
+++ compiler/ayuc_parser/src/ty.rs
@@ -1,17 +1,51 @@
 use ayuc_ast::{Ty, TyKind};
+use ayuc_lexer::{
+    stream::TokenStream,
+    token::{Delimiter, StructuredToken, TokenKind},
+};
 
 use crate::{PResult, Parser};
 
 impl Parser<'_, '_, '_> {
+    fn parse_tuple_ty(&mut self) -> PResult<Ty> {
+        let StructuredToken::Delimited(span, Delimiter::Parenthesis, tokens) =
+            self.require_token()?
+        else {
+            unreachable!()
+        };
+
+        let mut inner = self.branch(TokenStream::new(tokens));
+        let mut parts = Vec::new();
+        let mut expect_another = true;
+
+        while expect_another && !inner.stream.is_exhausted() {
+            parts.push(inner.parse_ty()?);
+
+            expect_another = inner.maybe(TokenKind::Comma);
+        }
+
+        Ok(Ty {
+            id: self.node_id_allocator.allocate(),
+            span: *span,
+            kind: TyKind::Tuple(parts),
+        })
+    }
+
+    fn parse_path_ty(&mut self) -> PResult<Ty> {
+        let path = self.parse_path()?;
+
+        Ok(Ty {
+            id: self.node_id_allocator.allocate(),
+            span: path.span,
+            kind: TyKind::Path(path),
+        })
+    }
+
     pub fn parse_ty(&mut self) -> PResult<Ty> {
-        if let Ok(path) = self.parse_path() {
-            Ok(Ty {
-                id: self.node_id_allocator.allocate(),
-                span: path.span,
-                kind: TyKind::Path(path),
-            })
-        } else {
-            todo!()
+        match self.stream.first() {
+            Some(StructuredToken::Delimited(_, Delimiter::Parenthesis, _)) => self.parse_tuple_ty(),
+            Some(StructuredToken::Token(_)) => self.parse_path_ty(),
+            _ => todo!(),
         }
     }
 }
