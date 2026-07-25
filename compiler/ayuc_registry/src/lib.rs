5b948511caf6b22531ec664d032c88c6a165556f
BaxoPlenty <60827021+BaxoPlenty@users.noreply.github.com>
2026-07-25 11:17:18 +0200

    feat(registry): add module registry

---
+++ compiler/ayuc_registry/src/lib.rs
@@ -0,0 +1,44 @@
+//! This module provides a registry for storing relevant information tied to a ModuleId.
+//! It's used to retrieve module information from a [ModuleId] and/or absolute file path.
+
+use std::collections::HashMap;
+
+use ayuc_ast::Ast;
+use ayuc_hir::Module;
+use ayuc_id::ModuleId;
+use slotmap::{SecondaryMap, SlotMap};
+
+/// A registry containing relevant information for modules such as their [Ast], [Module] and module dependencies.
+/// It also has a field to look up [`ModuleId`]s by their absolute file path.
+#[derive(Default)]
+pub struct ModuleRegistry {
+    /// A [SlotMap] containing an AST for a module. May be [`None`] if the file couldn't be parsed.
+    pub trees: SlotMap<ModuleId, Option<Ast>>,
+    /// A [SecondaryMap] storing the HIR module for a [`ModuleId`].
+    pub modules: SecondaryMap<ModuleId, Module>,
+    /// A [SecondaryMap] that has lists of all [`ModuleId`]s that a module is dependant of.
+    pub dependencies: SecondaryMap<ModuleId, Vec<ModuleId>>,
+
+    /// A [HashMap] that ties all existing [`ModuleId`]s to their absolute file path.
+    pub id_by_path: HashMap<String, ModuleId>,
+}
+
+impl ModuleRegistry {
+    /// Adds an Ast to the registry and returns the associated [ModuleId].
+    pub fn add_ast(&mut self, path: String, ast: Ast) -> ModuleId {
+        let id = self.trees.insert(Some(ast));
+
+        self.id_by_path.insert(path, id);
+
+        id
+    }
+
+    /// Adds an Ast to the registry that failed to parse and returns the associated [ModuleId].
+    pub fn add_failed_ast(&mut self, path: String) -> ModuleId {
+        let id = self.trees.insert(None);
+
+        self.id_by_path.insert(path, id);
+
+        id
+    }
+}
