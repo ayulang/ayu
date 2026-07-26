61c9f6965d0460ca2d833e56f2d49d4670841359
BaxoPlenty <60827021+BaxoPlenty@users.noreply.github.com>
2026-07-26 14:47:33 +0200

    feat(session): register item types

--- compiler/ayuc_session/src/session.rs
+++ compiler/ayuc_session/src/session.rs
@@ -1,11 +1,12 @@
 use std::collections::{HashMap, HashSet};
 
 use ayuc_id::{
+    TyId,
     ast::NodeId,
     hir::{DefId, LocalId},
 };
 use ayuc_type::interner::TypeInterner;
-use slotmap::SlotMap;
+use slotmap::{SecondaryMap, SlotMap};
 
 use crate::{ItemInfo, local::LocalInfo};
 
@@ -14,6 +15,7 @@ pub struct Session {
     pub interner: TypeInterner,
 
     items: SlotMap<DefId, ItemInfo>,
+    item_ty: SecondaryMap<DefId, TyId>,
 
     locals: SlotMap<LocalId, LocalInfo>,
     locals_by_id: HashMap<NodeId, LocalId>,
@@ -32,6 +34,14 @@ impl Session {
 
     pub fn register_item(&mut self, info: ItemInfo) -> DefId {
         self.items.insert(info)
+    }
+
+    pub fn register_item_ty(&mut self, id: DefId, ty: TyId) {
+        self.item_ty.insert(id, ty);
+    }
+
+    pub fn maybe_item_ty(&self, id: DefId) -> Option<TyId> {
+        self.item_ty.get(id).copied()
     }
 
     pub fn register_local(&mut self, info: LocalInfo) -> LocalId {
