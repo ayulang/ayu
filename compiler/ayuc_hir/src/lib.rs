2213d0b5a172f55ab6f0f2b40e09f60bc99fcfe1
BaxoPlenty <60827021+BaxoPlenty@users.noreply.github.com>
2026-08-16 20:29:26 +0200

    refactor(hir): remove redundant local module

--- compiler/ayuc_hir/src/lib.rs
+++ compiler/ayuc_hir/src/lib.rs
@@ -1,13 +1,11 @@
 pub mod def;
 pub mod expr;
-pub mod local;
 pub mod module;
 pub mod pat;
 pub mod stmt;
 
 pub use def::*;
 pub use expr::*;
-pub use local::*;
 pub use module::*;
 pub use pat::*;
 pub use stmt::*;
